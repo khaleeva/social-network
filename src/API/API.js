b0da8aa9ba0b92dc67d12977c221ca948ac66051
Eugenia Khaleeva <khaleeva20990@gmail.com>
2022-05-23 21:33:38 +0200

    fix api headers

--- src/API/API.js
+++ src/API/API.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
         'API-KEY': 'b429dd9e-7387-49ed-86ad-9db5be2725b1',
-        'Access-Control-Allow-Origin': 'https://khaleeva.github.io'
+        'Origin': 'https://khaleeva.github.io/'
     }
 })
 
